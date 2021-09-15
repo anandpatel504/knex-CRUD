ceeeff067b506de04e9b8992267195e535c80bb1
anandpatel504 <anand@navgurukul.org>
2021-09-15 12:19:41 +0530

    sample.env file added

--- Databases/db.js
+++ Databases/db.js
@@ -1,24 +1,25 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host: process.env.db_host,
-        user:process.env.db_user ,
-        password:process.env.db_password ,
-        database:process.env.db
-    },
-    useNullAsDefault: true
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+  useNullAsDefault: true,
 });
-knex.schema.createTable("Users", (table) => {
-    table.increments('id').primary();
-    table.string('Name')
-    table.string('email')
-    table.string('password');
-})
-    .then((data) => {
-        console.log("Table Created");
-    })
-    .catch((err) => {
-        console.log("Table Already Exist!!");
-    })
+knex.schema
+  .createTable("Users", (table) => {
+    table.increments("id").primary();
+    table.string("Name");
+    table.string("email");
+    table.string("password");
+  })
+  .then((data) => {
+    console.log("Table Created");
+  })
+  .catch((err) => {
+    console.log("Table Already Exist!!");
+  });
 
 module.exports = knex;
